ba422f36f70031fcb8d29bb9fa3f4bba74016959
1Mishany1 <129252904+1Mishany1@users.noreply.github.com>
2023-04-24 22:51:03 +0500

    Add files via upload

---
+++ классы 1/классы 1/классы 1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include <Header.h>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	Header A ;
+	int first, second;
+	cout << "Введите цену  ";
+	cin >> first;
+	cout << "Введите количество товаров: ";
+	cin >> second;
+	A.Init(first, second);
+	
+	cout << A.ipart();
+	return 0;
+}
