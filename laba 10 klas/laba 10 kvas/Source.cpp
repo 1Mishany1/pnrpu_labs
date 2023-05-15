2ef17ae7ad91f6f5def0aa1abbf8007a727ed888
1Mishany1 <129252904+1Mishany1@users.noreply.github.com>
2023-05-15 14:34:25 +0500

    Add files via upload

---
+++ laba 10 klas/laba 10 kvas/Source.cpp
@@ -0,0 +1,59 @@
+﻿#include "Time.h"
+#include "file_work.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "ru");
+	Time p, p1, p2;
+	int k, c;
+	char file_name[30];
+	do
+	{ 
+		cout << "\n1. Make file";
+		cout << "\n2. Print file";
+		cout << "\n3. удалить запись из файла";
+		cout << "\n4. Добавить запись в файл ";
+		cout << "\n5. Изменить запись в файле";
+		cout << "\n0. Выход из меню \n";
+		cin >> c;
+		switch (c)
+		{
+		case 1: cout << "file name?"; cin >> file_name;
+			k = make_file(file_name);
+			if (k < 0)cout << "Can't make file";
+			break;
+		case 2: cout << "file name?"; cin >> file_name;
+			k = print_file(file_name);
+			if (k == 0)cout << "Empty file\n";
+			if (k < 0)cout << "Can't read file\n";
+			break;
+		case 3: cout << "file name?"; cin >> file_name;
+			int nom; cout << "nom?"; cin >> nom;
+			k = del_file(file_name, nom);
+			if (k < 0)cout << "Can't read file";
+			break;
+		case 4: cout << "Введите имя файла:";
+			cin >> file_name;
+			cout << "Введите количесвто новых элементов: ";
+			cin >> nom;
+			k = add_file(file_name, nom, p1);
+			if (k < 0) cout << "Невозможно!";
+			if (k == 0) k = add_end(file_name, p1);
+			break;
+		case 5: cout << "Введите имя файла: ";
+			cin >> file_name;
+			cout << "Введите номер файла,который нужно изменить: "; cin >> nom;
+			cout << "Новая информация :\n";
+			//Time p2;
+			cin >> p2;
+			k = change_file(file_name, nom, p2);
+			if (k < 0) cout << "\nНевозможно прочитать ";
+			if (k == 0) cout << "\nНет такой информации!";
+			break;
+		}
+	} while (c != 0);
+	return 0;
+}
---
+++ laba 10 klas/laba 10 kvas/file_work.h
@@ -0,0 +1,113 @@
+﻿#pragma once
+#include "Time.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+int make_file(const char* f_minutes)
+{
+	fstream stream(f_minutes, ios::out | ios::trunc);
+	if (!stream)return -1;
+	int n;
+	Time p;
+	cout << "Введите новое время : "; cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> p;
+		stream << p << "\n";
+	}
+	stream.close();
+	return n;
+}
+int print_file(const char* f_minutes)
+{
+	fstream stream(f_minutes, ios::in);
+	if (!stream)return -1;
+	Time p; int i = 0;
+	while (stream >> p)
+	{
+		cout << p << "\n";
+		i++;
+	}
+	stream.close();
+	return i;
+}
+int del_file(const char* f_minutes, int k)
+{
+	fstream temp("temp", ios::out);
+	fstream stream(f_minutes, ios::in);
+	if (!stream)return -1;
+	int i = 0; Time p;
+	while (stream >> p)
+	{
+		if (stream.eof())break;
+		i++;
+		
+		if (i != k) temp << p;
+	}
+	stream.close(); temp.close();
+	remove(f_minutes);
+	rename("temp", f_minutes);
+	return i;
+}
+int add_file(const char* f_minutes, int count, Time pp)
+{
+	fstream temp("temp", ios::out);
+	fstream stream(f_minutes, ios::in);
+	if (!stream)return -1;
+	Time p; int i = 0, l = 0;
+	while (stream >> p)
+	{
+		if (stream.eof())break;
+		i++;
+		if (i == 1)
+		{
+			for (int j = 0; j < count; j++)
+			{
+				Time new_Time;
+				cout << "Введите новое время" << endl;
+				cin >> new_Time;
+				temp << new_Time;
+				l++;
+			}
+		}
+		temp << p;
+	}
+	stream.close(); temp.close();
+	remove(f_minutes);
+	rename("temp", f_minutes);
+	return l;
+}
+int add_end(const char* f_minutes, Time pp)
+{
+	fstream stream(f_minutes, ios::app);
+	if (!stream)return -1;
+	stream << pp; 
+	return 1;
+}
+int change_file(const char* f_minutes, int k, Time pp)
+{
+	fstream temp("temp", ios::out);
+	fstream stream(f_minutes, ios::in);
+	if (!stream)return -1;
+	Time p; int i = 0, l = 0;
+	char x;
+	while (stream >> p)
+	{
+		if (stream.eof())break;
+		i++;
+		if (i == k)
+		{
+			cout << p << "Если хотите продолжить,подтвердите[y/n]?\n";
+			cin >> x;
+			if (x == 'n' || x == 'N')break;
+			temp << pp;
+			l++;
+		}
+		else temp << p;
+	}
+	stream.close(); temp.close();
+	remove(f_minutes);
+	rename("temp", f_minutes);
+	return l;
+}
