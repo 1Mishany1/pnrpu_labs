8a2816bf569fcf1cb5c21627f309ca1ab50f8e24
1Mishany1 <129252904+1Mishany1@users.noreply.github.com>
2023-06-08 23:03:32 +0500

    Add files via upload

---
+++ Коммивояжёр/Комивояжор/Source.cpp
@@ -0,0 +1,23 @@
+#include "Graph.h"
+#include <iostream>
+#include <glut.h>
+#include <stdio.h>
+#include <vector>
+#include <sstream>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	setlocale(LC_ALL, "rus");
+	glutInit(&argc, argv);
+	graph = makeGraph();
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA);
+	glutInitWindowSize(1350, 730);
+	glutCreateWindow("Graph");
+	WinW = glutGet(GLUT_WINDOW_WIDTH);
+	WinH = glutGet(GLUT_WINDOW_HEIGHT);
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMouseFunc(mouseClick);
+	glutMainLoop();
+	return 0;
+}
